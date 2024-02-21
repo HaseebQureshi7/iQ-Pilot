74691ffe6f55b73aa49b7f24fb08e54129fb826f
Sheikh Umair <umair.najeeb@iquasar.com>
2024-02-21 19:12:50 +0530

    Refactor : Refactored Some Code In userModel

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,29 +1,30 @@
 const User = require("../models/userModel.js");
+const AppError = require("../util/AppError.js");
 const { catchAsync } = require("../util/catchAsync.js");
 
 exports.getAllUsers = catchAsync(async function (req, res) {
   const users = await User.find({});
   if (!users) {
-    return res.status(404).json({ msg: "No users found!" });
+    return res.status(404).json({ message: "No users found!" });
   }
   res
     .status(200)
-    .json({ msg: "All Users Found", results: users.length, users });
+    .json({ message: "All Users Found", count: users.length, users });
 });
 
 exports.getUser = catchAsync(async function (req, res) {
   const { id } = req.params;
   const user = await User.findById({ _id: id });
   if (!user) {
-    return res.status(404).json({ msg: "No user found!" });
+    return res.status(404).json({ message: "No user found!" });
   }
-  res.status(200).json({ msg: "User Found", user });
+  res.status(200).json({ message: "User Found", user });
 });
 
 exports.deleteUser = catchAsync(async function (req, res) {
   const { id } = req.params;
-  const user = await User.findByIdAndDelete({ _id: id });
-  res.status(204).json({ msg: "User Deleted!" });
+  await User.findByIdAndDelete({ _id: id });
+  res.status(204).json({ message: "User Deleted!" });
 });
 
 exports.updateUser = catchAsync(async function (req, res) {
@@ -33,13 +34,17 @@ exports.updateUser = catchAsync(async function (req, res) {
     runValidators: true,
   });
   if (!user) {
-    return res.status(404).json({ msg: "No user found!" });
+    return res.status(404).json({ message: "No user found!" });
   }
-  res.status(200).json({ msg: "User Updated!", user });
+  res.status(200).json({ message: "User Updated!", user });
 });
 
-exports.getAllEmployees = catchAsync(async (req, res) => {
+exports.getAllEmployees = catchAsync(async (req, res, next) => {
   const employees = await User.find({ role: "employee" });
+
+  if (!employees) {
+    return next(new AppError("No Employees Found", 404));
+  }
 
   res.status(200).json({
     messages: "Employees Found",
@@ -50,6 +55,10 @@ exports.getAllEmployees = catchAsync(async (req, res) => {
 
 exports.getAllDrivers = catchAsync(async (req, res) => {
   const drivers = await User.find({ role: "driver" });
+
+  if (!drivers) {
+    return next(new AppError("No drivers Found", 404));
+  }
 
   res.status(200).json({
     messages: "Drivers Found",
