673ddfc3b73f3f342e868d861bb65a22ea89efa1
Sheikh Umair <umair.najeeb@iquasar.com>
2024-02-21 18:52:11 +0530

    Feat : Created APIs (GetAllEmployees, GetAllDrivers)

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -11,22 +11,22 @@ exports.getAllUsers = catchAsync(async function (req, res) {
     .json({ msg: "All Users Found", results: users.length, users });
 });
 
-exports.getUser = async function (req, res) {
+exports.getUser = catchAsync(async function (req, res) {
   const { id } = req.params;
   const user = await User.findById({ _id: id });
   if (!user) {
     return res.status(404).json({ msg: "No user found!" });
   }
   res.status(200).json({ msg: "User Found", user });
-};
+});
 
-exports.deleteUser = async function (req, res) {
+exports.deleteUser = catchAsync(async function (req, res) {
   const { id } = req.params;
   const user = await User.findByIdAndDelete({ _id: id });
   res.status(204).json({ msg: "User Deleted!" });
-};
+});
 
-exports.updateUser = async function (req, res) {
+exports.updateUser = catchAsync(async function (req, res) {
   const { id } = req.params;
   const user = await User.findByIdAndUpdate({ _id: id }, req.body, {
     new: true,
@@ -36,4 +36,24 @@ exports.updateUser = async function (req, res) {
     return res.status(404).json({ msg: "No user found!" });
   }
   res.status(200).json({ msg: "User Updated!", user });
-};
+});
+
+exports.getAllEmployees = catchAsync(async (req, res) => {
+  const employees = await User.find({ role: "employee" });
+
+  res.status(200).json({
+    messages: "Employees Found",
+    count: employees.length,
+    employees,
+  });
+});
+
+exports.getAllDrivers = catchAsync(async (req, res) => {
+  const drivers = await User.find({ role: "driver" });
+
+  res.status(200).json({
+    messages: "Drivers Found",
+    count: drivers.length,
+    drivers,
+  });
+});
