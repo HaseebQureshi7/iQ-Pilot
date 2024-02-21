65de773ecaaba39c7f81d7dd446b27f8d5285d54
SaaibQ <saaib.qazi@iquasar.com>
2024-02-21 19:25:39 +0530

    FEAT:Added restrictTo userRoutes.

--- server/routes/routeRoutes.js
+++ server/routes/routeRoutes.js
@@ -13,8 +13,12 @@ const router = express.Router();
 router
   .route("/")
   .get(protect, restrictTo("admin"), getAllRoutes)
-  .post(createRoute);
+  .post(protect, restrictTo("admin"), createRoute);
 
-router.route("/:id").get(getRoute).delete(deleteRoute).patch(updateRoute);
+router
+  .route("/:id")
+  .get(protect, restrictTo("admin"), getRoute)
+  .delete(protect, restrictTo("admin"), deleteRoute)
+  .patch(protect, restrictTo("admin"), updateRoute);
 
 module.exports = router;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,14 +7,19 @@ const {
   getAllEmployees,
   getAllDrivers,
 } = require("../controllers/userController");
+const { restrictTo, protect } = require("../middleware/authMiddleware.js");
 
 const router = express.Router();
 
-router.route("/employees").get(getAllEmployees);
-router.route("/drivers").get(getAllDrivers);
+router.route("/employees").get(protect, restrictTo("admin"), getAllEmployees);
+router.route("/drivers").get(protect, restrictTo("admin"), getAllDrivers);
 
-router.route("/").get(getAllUsers);
+router.route("/").get(protect, restrictTo("admin"), getAllUsers);
 
-router.route("/:id").get(getUser).delete(deleteUser).patch(updateUser);
+router
+  .route("/:id")
+  .get(protect, restrictTo("admin"), getUser)
+  .delete(protect, restrictTo("admin"), deleteUser)
+  .patch(protect, restrictTo("admin"), updateUser);
 
 module.exports = router;
