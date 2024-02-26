380285546def0dc1d64c7fc737e2771eddc89446
Sheikh Umair <umair.najeeb@iquasar.com>
2024-02-26 17:08:42 +0530

    Refactored: app.js

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, "static/profilePictures")));
-app.use(express.json({ limit: "2mb" }));
+app.use(express.json({ limit: "10mb" }));
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -25,7 +25,7 @@ app.use(
   })
 );
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true, limit: "2mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use("/api/v1/auth", authRouter);
 
 app.use("/api/v1/users", userRouter);
