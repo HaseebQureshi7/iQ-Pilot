ead00b5340bbada3c7c0578a3d293355d883b4a9
Sheikh Umair <umair.najeeb@iquasar.com>
2024-02-29 20:21:40 +0530

    BASE URL UPDATED

--- client/src/api/useAxios.ts
+++ client/src/api/useAxios.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
+import baseURL from "../utils/baseURL";
 
 const useAxios = axios.create({
-    baseURL: "http://localhost:5000/api/v1/",
-    withCredentials: true
-})
+  baseURL: `${baseURL}api/v1/`,
+  withCredentials: true,
+});
 
-export default useAxios
+export default useAxios;
--- client/src/utils/baseURL.ts
+++ client/src/utils/baseURL.ts
@@ -1,3 +1,5 @@
-const baseURL = "http://localhost:5000/";
+// const baseURL = "http://localhost:5000/";
+// const baseURL = "https://iqserver.vercel.app/";
+const baseURL = "https://iqserver.onrender.com/";
 
 export default baseURL;
