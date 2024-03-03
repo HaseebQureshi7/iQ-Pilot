68cc5722956a625de6e5efcabcd514dcf7b3b32f
Haseeb Qureshi - IQSS <143264149+haseebq-iqss@users.noreply.github.com>
2024-03-04 00:18:08 +0530

    TS-Complie fix.

--- client/src/router/MainRouter.tsx
+++ client/src/router/MainRouter.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import Landing from "../pages/Landing";
 import Signup from "../pages/Signup/Signup";
-import { IOSExpand, FadeIn, SlideInOut } from "../animations/transitions";
+import { IOSExpand, SlideInOut } from "../animations/transitions";
 import AdminDashboard from "../pages/AdminDashboard/AdminDashboard";
 import DriverDashboard from "../pages/DriverDashboard/DriverDashboard";
 import EmployeeDashboard from "../pages/EmployeeDashboard/EmployeeDashboard";
@@ -24,9 +24,9 @@ function MainRouter() {
   const location = useLocation();
 
   const isBaseRoute: boolean =
-  !location.pathname.includes("admin") &&
-  !location.pathname.includes("employee") &&
-  !location.pathname.includes("driver");
+    !location.pathname.includes("admin") &&
+    !location.pathname.includes("employee") &&
+    !location.pathname.includes("driver");
 
   return (
     <AnimatePresence mode="wait">
@@ -96,7 +96,14 @@ function MainRouter() {
         {/* DRIVER ROUTER */}
         {userData?.role === "driver" && (
           <>
-            <Route path="/driver" element={<IOSExpand><DriverLayout /></IOSExpand>}>
+            <Route
+              path="/driver"
+              element={
+                <IOSExpand>
+                  <DriverLayout />
+                </IOSExpand>
+              }
+            >
               <Route index element={<DriverDashboard />} />
               <Route path="startRoute" element={<StartRoute />} />
               <Route path="routeCompleted" element={<RouteCompleted />} />
@@ -105,7 +112,14 @@ function MainRouter() {
         )}
         {/* EMPLOYEE ROUTER */}
         {userData?.role === "employee" && (
-          <Route path="/employee" element={<IOSExpand><EmployeeDashboard /></IOSExpand>} />
+          <Route
+            path="/employee"
+            element={
+              <IOSExpand>
+                <EmployeeDashboard />
+              </IOSExpand>
+            }
+          />
         )}
       </Routes>
     </AnimatePresence>
