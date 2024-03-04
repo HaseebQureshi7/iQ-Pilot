76b329ad4c89ffb56b286ed3a90f8557eb49d44c
Haseeb Qureshi - IQSS <143264149+haseebq-iqss@users.noreply.github.com>
2024-03-04 23:16:41 +0530

    Extra CORS config removed +++

--- server/app.js
+++ server/app.js
@@ -20,15 +20,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(
   cors({
     credentials: true,
-    optionSuccessStatus: 200,
-    Headers: true,
     exposedHeaders: 'Set-Cookie',
-    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-    allowedHeaders: [
-      'Access-Control-Allow-Origin',
-      'Content-Type',
-      'Authorization'
-    ],
     origin: ["http://localhost:5173", "https://ipilot.vercel.app"],
   })
 );
