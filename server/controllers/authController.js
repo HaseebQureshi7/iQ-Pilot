53e899a621a1d8cc0b42e462d2f7e36ad0fe372f
Sheikh Umair <umair.najeeb@iquasar.com>
2024-03-05 14:47:54 +0530

    new cookie modifications

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -90,7 +90,7 @@ const login = catchAsync(async (req, res, next) => {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     secure: true,
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: "lax",
   });
 
   res.status(200).json({
